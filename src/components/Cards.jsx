233e20675a5d2118751e734598c1467aed07463a
Fernando Faria <fernandoafaria@yahoo.com>
2018-10-08 13:35:54 -0400

    phase 2 complete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,41 +2,81 @@ import React from 'react';
 
 
 class Cards extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-             info: []
+            loadFilms: false,
+            loadPeople: false,
+            info: []
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://ghibliapi.herokuapp.com/films')
-        .then(res => res.json())
-        .then(obj => {
-            
-            this.setState({info: [...obj]})
-            console.log(this.state.info);
+            .then(res => res.json())
+            .then(obj => {
+
+                this.setState({ info: [...obj] })
+                console.log(this.state.info);
+            })
+
+    }
+    handleClickDisplayMovie() {
+        this.setState({
+            loadFilms: true,
+            loadPeople: false
         })
-    
+    }
+    handleClickClearFilms() {
+        this.setState({
+            loadFilms: false,
+            loadPeople: false
+        })
     }
 
     render() {
-        return (
-            <div>
-                {this.state.info.map((x) => {
-                    return (
-                    <div key={x.id} className="card border-info mb-3" style={{maxWidth: '35rem'}}>
-                        <div className="card-header">{x.title}</div>
-                        <div className="card-body">
-                            <h4 className="card-title">Directed by {x.director}</h4>
-                            <p className="card-text">{x.description}</p>
-                        </div>
-                    </div>
-                    )
-                })}
-            </div>
-        )
 
-}
+        if (this.state.loadFilms === true) {
+            return (
+                <div>
+                    <div style={{ margin: '25px' }}>
+                        <button className="btn btn-primary mb-4" onClick={() => this.handleClickClearFilms()}>Clear Films</button>
+                    </div>
+
+
+                    {this.state.info.map((x) => {
+                        return (
+                            <div key={x.id} className="card border-info mb-3" style={{ maxWidth: '50rem' }}>
+                                <div className="card-header">{x.title}
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Directed by {x.director}</h4>
+                                    <p className="card-text">{x.description}</p>
+                                </div>
+                                </div>
+                        )
+                    })}
+                </div>
+
+            )
+        } else {
+
+            return (
+                <div style={{margin: '25px'}}>
+                    <div>
+                        <button className="btn btn-primary mb-4" onClick={() => this.handleClickDisplayMovie()}>Load Films</button>
+                    </div>
+
+                </div>
+            )
+        }
+
+
+
+
+
+
+
+    }
 }
 
 export default Cards;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Cards from './components/Cards'
 
 
 const App = () => {
+   
 
     return (
         <div>
