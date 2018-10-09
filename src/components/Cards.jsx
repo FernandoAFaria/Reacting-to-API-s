61495148ff8041f25cc39a0235b7342d2d26214f
Fernando Faria <fernandoafaria@yahoo.com>
2018-10-09 09:58:46 -0400

    finished working with API in React

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../logo.png'
 
 
 class Cards extends React.Component {
@@ -7,7 +8,8 @@ class Cards extends React.Component {
         this.state = {
             loadFilms: false,
             loadPeople: false,
-            info: []
+            movieInfo: [],
+            peopleInfo: []
         }
     }
     componentDidMount() {
@@ -15,15 +17,25 @@ class Cards extends React.Component {
             .then(res => res.json())
             .then(obj => {
 
-                this.setState({ info: [...obj] })
-                console.log(this.state.info);
+                this.setState({ movieInfo: [...obj] })
+                
             })
-
+        fetch('https://ghibliapi.herokuapp.com/people')
+            .then(res => res.json())
+            .then(obj => {
+                this.setState({ peopleInfo: [...obj]})
+            })    
     }
     handleClickDisplayMovie() {
         this.setState({
             loadFilms: true,
             loadPeople: false
+        })
+    }
+    handleClickDisplayPeople() {
+        this.setState({
+            loadFilms: false,
+            loadPeople: true
         })
     }
     handleClickClearFilms() {
@@ -35,36 +47,54 @@ class Cards extends React.Component {
 
     render() {
 
-        if (this.state.loadFilms === true) {
-            return (
-                <div>
-                    <div style={{ margin: '25px' }}>
-                        <button className="btn btn-primary mb-4" onClick={() => this.handleClickClearFilms()}>Clear Films</button>
-                    </div>
+        let renderMovieInfo = (
+            this.state.movieInfo.map((x) => {
+                    return (
+                        <div key={x.id} className="card border-info mb-3" style={{ minWidth: '35rem' }}>
+                            <div className="card-header">Film Title: {x.title}
+                            </div>
+                            <div className="card-body">
+                                <h4 className="card-title">Directed by {x.director}</h4>
+                                <p className="card-text">{x.description}</p>
+                            </div>
+                        </div>
+                    )
+                })
+        );
 
+        let renderPeopleInfo = (
+            
+                this.state.peopleInfo.map((person) => {
 
-                    {this.state.info.map((x) => {
-                        return (
-                            <div key={x.id} className="card border-info mb-3" style={{ maxWidth: '50rem' }}>
-                                <div className="card-header">{x.title}
-                                </div>
-                                <div className="card-body">
-                                    <h4 className="card-title">Directed by {x.director}</h4>
-                                    <p className="card-text">{x.description}</p>
-                                </div>
-                                </div>
-                        )
-                    })}
-                </div>
+                    return (
+                        <div key={person.id} className="card border-primary mb-3" style={{ minWidth: '20rem' }} >
+                            <div className="card-header">{person.id}</div>
+                            <div className="card-body">
+                                <h4 className="card-title">{person.name}</h4>
+                                <p className="card-text" >{person.age}, {person.gender}
+                                    <button style={{ marginLeft: '25px' }} className="btn btn-primary" onClick={() => window.open(person.url)}>Open Info</button></p>
+                            </div>
+                        </div>
+                    )
+                })
+        );
 
-            )
-        } else {
+   
 
             return (
                 <div style={{margin: '25px'}}>
-                    <div>
-                        <button className="btn btn-primary mb-4" onClick={() => this.handleClickDisplayMovie()}>Load Films</button>
+                <div style={{textAlign: 'center', marginBottom: '35px'}}>
+                    <img src={logo} alt=""/>
+                </div>
+                    <div style={{ textAlign: 'center' }}>
+                        <button style={{marginRight: '10px'}} className="btn btn-primary mb-4" onClick={() => this.handleClickDisplayMovie()}>Load Films</button>
+                        <button className="btn btn-primary mb-4" onClick={() => this.handleClickDisplayPeople()}>Load People</button>
+                        <button style={{ marginLeft: '10px' }} className="btn btn-primary mb-4" onClick={() => this.handleClickClearFilms()}>Clear</button>
                     </div>
+                    {this.state.loadFilms ? renderMovieInfo : ""   }
+                    {this.state.loadPeople ? renderPeopleInfo : ""}
+                
+                   
 
                 </div>
             )
@@ -77,6 +107,6 @@ class Cards extends React.Component {
 
 
     }
-}
+
 
 export default Cards;
